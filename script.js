2973e53294e9ad1e2e836cf9612b6d2f37ab8884
davissears <davisrandsears@gmail.com>
2025-08-22 00:40:34 -0700

    Computer choice logical
    getComputerChoice fucntion added
    to script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const choice = Math.random();
+function getComputerChoice(choice) {
+  if (choice < 0) {
+    return "rock";
+  } else if (result === 0) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
