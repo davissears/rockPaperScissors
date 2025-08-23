9b1d698e0429a494eb913ac4c3a58db87568f125
davissears <davisrandsears@gmail.com>
2025-08-22 19:50:15 -0700

    user choice logic
    created getHumanChoice() fucntion.
        //prompts user to type a selection
        //returns valid entries in lowercase
        //otherwise user is re-prompted
        //console.log() test: PASS

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const choice = Math.random();
-function getComputerChoice(choice) {
-  if (choice < 0) {
+//*
+let computerChoice = Math.random();
+function getComputerChoice(computerChoice) {
+  if (computerChoice < 0) {
     return "rock";
-  } else if (result === 0) {
+  } else if (computerChoice === 0) {
     return "paper";
   } else {
     return "scissors";
   }
 }
+console.log(getComputerChoice(computerChoice));
+
+let userChoice = prompt("rock || paper || scissors");
+
+function getHumanChoice(userChoice) {
+  if (userChoice.toLowerCase() === "rock" || "paper" || "scissors") {
+    return userChoice.toLowerCase();
+  } else prompt("rock || paper || scissors");
+  return userChoice;
+}
+console.log(getHumanChoice(userChoice));
