94b828f1ba330ec02a04fc75e0bd49a6cf2dfc87
davissears <davisrandsears@gmail.com>
2025-08-23 20:33:06 -0700

    round logic core functionality
    created variables, and core logic for playRound()
        //function determines winner for 2 potential
        outcomes
        //increments scoring
    
    bug fix in getComputerChoice() logic
        //adjusted if/else comparison values to return
        a more random result

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-//*
+//* computerChoice logic
 let computerChoice = Math.random();
 function getComputerChoice(computerChoice) {
-  if (computerChoice < 0) {
+  if (computerChoice <= 0.333) {
     return "rock";
-  } else if (computerChoice === 0) {
+  } else if (computerChoice <= 0.666) {
     return "paper";
   } else {
     return "scissors";
@@ -11,16 +11,46 @@ function getComputerChoice(computerChoice) {
 }
 console.log(getComputerChoice(computerChoice));
 
+//*userChoice logic
 let userChoice = prompt("rock || paper || scissors");
-
 function getHumanChoice(userChoice) {
-  if (userChoice.toLowerCase() === "rock" || "paper" || "scissors") {
-    return userChoice.toLowerCase();
-  } else prompt("rock || paper || scissors");
-  return userChoice;
+  if (userChoice === "rock" || "paper" || "scissors") return userChoice;
 }
 console.log(getHumanChoice(userChoice));
 
 //*score variables
+var computer = "computer";
+var human = "human";
 let humanScore = 0;
 let computerScore = 0;
+var currentWinner;
+let roundWinner = Math.max(humanScore, computerScore);
+//* single round logic
+function playRound(humanChoice, computerChoice) {
+  if ((humanChoice = "rock") && (computerChoice = "paper")) {
+    currentWinner = computer;
+    return currentWinner;
+  } else if ((humanChoice = "rock") && (computerChoice = "scissors")) {
+    currentWinner = human;
+    return currentWinner;
+  }
+  console.log(currentWinner);
+  if ((currentWinner = human)) {
+    ++humanScore;
+  } else if ((currentWinner = computer)) {
+    ++computerScore;
+  }
+  {
+    return currentWinner;
+  }
+}
+console.log(currentWinner, roundWinner);
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+{
+}
+console.log(currentWinner, roundWinner);
+
+
