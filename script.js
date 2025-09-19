6394d13dd894d7f43986b509c72075ce952e1752
davissears <davisrandsears@gmail.com>
2025-09-18 22:27:19 -0700

    refactor: Improve button functionality and update outcome display in UI

--- script.js
+++ script.js
@@ -10,100 +10,70 @@ function getComputerChoice(computerChoice) {
   return computerChoice;
 }
 
-function getHumanChoice(humanChoice) {
-  console.log(humanChoice);
+/*function getHumanChoice(humanChoice) {
   if (
     humanChoice === "rock" ||
     humanChoice === "paper" ||
     humanChoice === "scissors"
   )
     return humanChoice;
-}
-function playRound() {
-  const humanChoice = getHumanChoice(humanChoice);
-  const computerChoice = getComputerChoice();
-  let computer = "computer";
-  let human = "human";
-  let tie = "tie";
-  let currentWinner;
-  humanChoice;
-  computerChoice;
-}
+}*/
 
-/*function getHumanChoice() {
-  //* toggle one of the two following lines to switch between browser and debugger testing
-  let userInput = window.prompt("rock || paper || scissors");
-  //*userInput = "ROCK";
-  let input = userInput.toLowerCase();
-  if (input === "rock" || input === "paper" || input === "scissors")
-    return input;
-}
-
-
-
-function playGame() {
+function playGame(humanChoice) {
   let humanScore = 0;
   let computerScore = 0;
-  //* single round logic
+  const userChoice = humanChoice;
+
   function playRound() {
-    const humanChoice = getHumanChoice();
     const computerChoice = getComputerChoice();
     let computer = "computer";
     let human = "human";
     let tie = "tie";
     let currentWinner;
-    humanChoice;
-    computerChoice;
-  }
-  //* humanChoice = rock
-  if (humanChoice === "rock" && computerChoice === "paper") {
-    currentWinner = computer;
-    console.log("Paper cover rock!");
-  } else if (humanChoice === "rock" && computerChoice === "scissors") {
-    currentWinner = human;
-    console.log("Rock breaks scissors!");
-  }
-  //*humanChoice = paper
-  else if (humanChoice === "paper" && computerChoice === "scissors") {
-    currentWinner = computer;
-    console.log("Scissors cut paper!");
-  } else if (humanChoice === "paper" && computerChoice === "rock") {
-    currentWinner = human;
-    console.log("Paper covers rock!");
-  }
-  //*humanChoice = scissors
-  else if (humanChoice === "scissors" && computerChoice === "rock") {
-    currentWinner = computer;
-    console.log("Rock breaks scissors!");
-  } else if (humanChoice === "scissors" && computerChoice === "paper") {
-    currentWinner = human;
-    console.log("Scissors cut paper!");
-  }
-  //*tie
-  else if (humanChoice === computerChoice) {
-    currentWinner = tie;
-    console.log("It's a Tie!");
-  }
+    //* userChoice = rock
+    if (userChoice === "rock" && computerChoice === "paper") {
+      currentWinner = computer;
+      console.log("Paper cover rock!");
+    } else if (userChoice === "rock" && computerChoice === "scissors") {
+      currentWinner = human;
+      console.log("Rock breaks scissors!");
+    }
+    //*userChoice = paper
+    else if (userChoice === "paper" && computerChoice === "scissors") {
+      currentWinner = computer;
+      console.log("Scissors cut paper!");
+    } else if (userChoice === "paper" && computerChoice === "rock") {
+      currentWinner = human;
+      console.log("Paper covers rock!");
+    }
+    //*userChoice = scissors
+    else if (userChoice === "scissors" && computerChoice === "rock") {
+      currentWinner = computer;
+      console.log("Rock breaks scissors!");
+    } else if (userChoice === "scissors" && computerChoice === "paper") {
+      currentWinner = human;
+      console.log("Scissors cut paper!");
+    }
+    //*tie
+    else if (userChoice === computerChoice) {
+      currentWinner = tie;
+      console.log("It's a Tie!");
+    }
 
-  //* winner logic
-  if (currentWinner === human) {
-    ++humanScore;
-    console.log("You win this round!");
-  } else if (currentWinner === computer) {
-    ++computerScore;
-    console.log("You lose this round!");
+    //* winner logic
+    if (currentWinner === human) {
+      ++humanScore;
+      console.log("You win this round!");
+    } else if (currentWinner === computer) {
+      ++computerScore;
+      console.log("You lose this round!");
+    }
+    console.log(computerScore, humanScore);
+    return;
   }
-  console.log(computerScore, humanScore);
-  return;
-
   //Round Logic
 
-  //for (let i = 0; i < 5; i++)
-  playRound();
-  {
+  for (let i = 0; i < 5; i++) {
+    playRound();
   }
-  playGame();
 }
-
-//exports
-*/
--- ui.js
+++ ui.js
@@ -5,21 +5,25 @@ listener to the buttons that call your playRound function with
 the correct playerSelection every time a button is clicked.
  (you can keep the console.logs for this step)*/
 
-//button event listeners
-// Get all buttons for human selection
-const btnChoices = document.querySelectorAll("button");
-const outcomeBox = document.getElementsByClassName("#outcome");
+const btnChoices = document.querySelectorAll("button"); // button selector
 
+//get text from buttons
+function addTextNode(text) {
+  const outcomeText = document.createTextNode(text); //
+  const outcome = document.getElementById("outcome");
+
+  outcome.appendChild(outcomeText);
+}
+
+// react to user selection
 btnChoices.forEach((button) => {
   button.addEventListener("click", function (e) {
-    let humanChoice = this.className;
-    getHumanChoice(humanChoice);
+    const lineBreak = document.createElement("br"); //define lineBreak
+    let humanChoice = this.className; //defines variable to pass to getHumanChoice
+    addTextNode(`You Chose ${e.target.textContent} `); //adds content to .outcome div
+    outcome.appendChild(lineBreak); // adds linebreak to .outcome div
+    
+    playGame(humanChoice);
   });
-
-  function updateBox() {
-    const outcomeBox = document.createElement("p");
-    const text = document.createTextNode(this.className);
-    outcomeBox.appendChild(text);
-  }
 });
 // playGame() logs to #outcome
